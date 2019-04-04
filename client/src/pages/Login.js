d5019a9f937ef55d2d3e23a6bd384539f64a65c9
Lucas Werner <lwerner27@gmail.com>
2019-04-04 14:13:43 -0500

    setup basics of the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            username: "",
+            password: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    handleClick() {
+        console.log(this.state)
+    }
+
     render() {
         return (
             <Container>
@@ -11,13 +31,13 @@ export default class Login extends Component {
                         <Form>
                             <FormGroup>
                                 <Label for="username">Username</Label>
-                                <Input type="username" name="username" id="username" placeholder="with a placeholder" />
+                                <Input type="username" name="username" id="username" placeholder="with a placeholder" onChange={this.handleChange}/>
                             </FormGroup>
                             <FormGroup>
                                 <Label for="password">Password</Label>
-                                <Input type="password" name="password" id="password" placeholder="password placeholder" />
+                                <Input type="password" name="password" id="password" placeholder="password placeholder" onChange={this.handleChange}/>
                             </FormGroup>
-                            <Button color="warning" block>Submit</Button>
+                            <Button color="warning" block onClick={this.handleClick}>Submit</Button>
                             <Button color="success" block>Register</Button>
                         </Form>
                     </Col>
