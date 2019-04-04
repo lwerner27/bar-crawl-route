6cae64d557e4e8c88bb87946bd91d8e9e591baa6
Lucas Werner <lwerner27@gmail.com>
2019-04-04 12:00:53 -0500

    added a AppNav component

---
+++ client/src/components/AppNav.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink } from 'reactstrap';
+
+export default class AppNav extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+  render() {
+    return (
+        <div>
+            <Navbar color="dark" dark expand="md">
+                <NavbarBrand href="/">Bar Crawl Routes</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/login">Login</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+  }
+}
