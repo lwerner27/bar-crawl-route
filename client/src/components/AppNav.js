a80c3c099fd89c4aae65f433ef6ec3f598d2ad0b
Lucas Werner <lwerner27@gmail.com>
2019-04-04 13:52:40 -0500

    removed custom styles

--- client/src/components/AppNav.js
+++ client/src/components/AppNav.js
@@ -24,28 +24,20 @@ export default class AppNav extends Component {
         });
     }
 
-  render() {
-    let { navStyles } = styles
-    return (
-        <div>
-            <Navbar style={ navStyles } dark expand="md">
+    render() {
+        return (
+            <Navbar color="primary" dark expand="md">
                 <NavbarBrand href="/">Bar Crawl Routes</NavbarBrand>
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav style={ navStyles } className="ml-auto" navbar>
+                    <Nav className="ml-auto" navbar>
                         <NavItem>
                             <NavLink href="/login">Login</NavLink>
                         </NavItem>
                     </Nav>
                 </Collapse>
             </Navbar>
-        </div>
-    )
-  }
-}
-
-const styles = {
-    navStyles: {
-        backgroundColor: "#009ddc"
+        )
     }
 }
+
