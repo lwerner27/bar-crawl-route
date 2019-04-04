44a53c45df5caeda9e030c3a19f9e3be46fa2209
Lucas Werner <lwerner27@gmail.com>
2019-04-04 14:24:20 -0500

    added some state and a method for mutating it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import AppNav from "./components/AppNav"
 import Login from "./pages/Login"
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props) 
+    this.state = {
+      userId: null,
+      username: null,
+      jwt: null
+    }
+    this.UPDATE_USER_INFO = this.UPDATE_USER_INFO.bind(this)
+  }
+
+  UPDATE_USER_INFO(userInfo) {
+    this.setState(userInfo)
+  }
 
   render() {
     return (
