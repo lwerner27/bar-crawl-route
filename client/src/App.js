2991009369e18748f80b5ad049d379500cc0a904
Lucas Werner <lwerner27@gmail.com>
2019-04-04 12:02:13 -0500

    added AppNav to the app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppNav from "./components/AppNav"
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+        <AppNav />
       </div>
     );
   }
