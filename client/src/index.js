8c2bdae341ff49fcac491e46311c01d5056c3e1a
Lucas Werner <lwerner27@gmail.com>
2019-04-04 13:53:03 -0500

    removed import of bootstrap and add import of bootswatch

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Bootswatch.css"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
