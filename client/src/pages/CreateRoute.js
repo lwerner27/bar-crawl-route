11da3474bdb799a675556e7b5ce179760c6ba6a2
Lucas Werner <lwerner27@gmail.com>
2019-04-05 09:48:06 -0500

    setup basic page structure

---
+++ client/src/pages/CreateRoute.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+export default class CreateRoute extends Component {
+  render() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>This the the Create Route page.</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+  }
+}
