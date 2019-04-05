3a689ff9f395624dc8d2c60e425ff89348b0d1a6
Lucas Werner <lwerner27@gmail.com>
2019-04-05 09:55:57 -0500

    reformmated code

--- client/src/pages/CreateRoute.js
+++ client/src/pages/CreateRoute.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 
 export default class CreateRoute extends Component {
-  render() {
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <h1>This the the Create Route page.</h1>
-                </Col>
-            </Row>
-        </Container>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            userId: null,
+            username: null,
+            jwt: null
+        }
+    }
+    
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>This is the Create Route page.</h1>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
