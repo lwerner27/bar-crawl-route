23e3173fe8b124f548d775a76f93789045832e3d
Lucas Werner <lwerner27@gmail.com>
2019-04-05 11:42:41 -0500

    added body parser and imported all routes

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 require('dotenv').config()
 const express = require("express")
+const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
+
+app.use(express.static('dist'))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log("Server listening on port: " + PORT)
