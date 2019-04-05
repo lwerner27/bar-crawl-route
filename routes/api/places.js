57c998231b8902e946518ef51c1c3c2a75082a74
Lucas Werner <lwerner27@gmail.com>
2019-04-05 11:41:59 -0500

    setup route for searching for places

--- routes/api/places.js
+++ routes/api/places.js
@@ -6,7 +6,11 @@ const apiKey = process.env.GOOGLE_KEY
 // const { getToken } = require("../../Shared")
 
 router.get("/search/:queryText", (req, res) => {
+    let { queryText } = req.params
     axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${queryText}&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,geometry&key=${apiKey}`)
+    .then((places) => {
+        res.json(places)
+    })
 })
 
 module.exports = router
